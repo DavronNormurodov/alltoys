216b3345dc24913413a39de5c19833e0c92061bf
Your Name <mrz.davron@gmail.com>
2021-01-22 17:35:03 +0500

    Create Address and Tag models and migrations

--- toys/models.py
+++ toys/models.py
@@ -6,14 +6,29 @@ class ActiveObjectsManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(is_active=True)
 
+class Address(models.Model):
+    street = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, null=True, blank=True)
+    zip_code = models.CharField(max_length=100, null=True, blank=True)
+    country = models.CharField(max_length=100, null=True, blank=True)
+
+
 class User(models.Model):
     is_active = models.BooleanField(default=True)
     first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    age = models.IntegerField()
+    last_name = models.CharField(max_length=50, null=True, blank=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
+    phone = models.CharField(max_length=100, null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    address = models.OneToOneField(Address, on_delete=models.PROTECT, null=True, blank=True)
 
     objects = models.Manager()
     active_objects = ActiveObjectsManager()
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
 
 class Toy(models.Model):
     name = models.CharField(max_length=100)
@@ -26,3 +41,4 @@ class Toy(models.Model):
     # # userni o'chirmoqchi bo'lsak oldin uni toylarini o'chirishimizni so'raydi
     # user = models.ForeignKey(User, related_name='toys', on_delete=models.PROTECT, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
+    tags = models.ManyToManyField(Tag, related_name='toys')
