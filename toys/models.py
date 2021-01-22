f17d5f0d308087bcdb7e5d87732a70432ddacfc4
Your Name <mrz.davron@gmail.com>
2021-01-22 16:53:46 +0500

    Create Toy model and migrations

--- toys/models.py
+++ toys/models.py
@@ -12,4 +12,17 @@ class User(models.Model):
     last_name = models.CharField(max_length=50)
     age = models.IntegerField()
 
+    objects = models.Manager()
     active_objects = ActiveObjectsManager()
+
+class Toy(models.Model):
+    name = models.CharField(max_length=100)
+    # user o'chirilsa toy ham o'chadi
+    user = models.ForeignKey(User, related_name='toys', on_delete=models.CASCADE, null=True, blank=True)
+
+    # # user o'chirilsa toylari null boladi
+    # user = models.ForeignKey(User, related_name='toys', on_delete=models.SET_NULL, null=True, blank=True)
+
+    # # userni o'chirmoqchi bo'lsak oldin uni toylarini o'chirishimizni so'raydi
+    # user = models.ForeignKey(User, related_name='toys', on_delete=models.PROTECT, null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
